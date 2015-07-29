cad9111d1c9d4369eb3deae6f58acf2d6e7e121b
EricZimmerman <saericzimmerman@gmail.com>
2015-07-29 10:14:03 -0600

    Split out parsing code from cmdline

---
+++ Amcache/Classes/FileEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amcache.Classes
+{
+    public class FileEntry
+    {
+        public string ProductName { get; }
+        public string FullPath { get; }
+        public string ProgramID { get; }
+        public string SHA1 { get; }
+        public string FileID { get; }
+        public string VolumeID { get; }
+
+        public DateTimeOffset VolumeLastWriteTimestamp { get; }
+        public DateTimeOffset LastWriteTimestamp { get; }
+
+
+        public DateTimeOffset LastModified2 { get; }
+
+        public FileEntry(string productName, string programID, string sha1, string fullPath, DateTimeOffset lastMod2, string volumeID, DateTimeOffset volumeLastWrite, string fileID, DateTimeOffset lastWrite)
+        {
+            ProductName = productName;
+            ProgramID = programID;
+            SHA1 = sha1.Substring(4);
+            FullPath = fullPath;
+            LastModified2 = lastMod2;
+            FileID = fileID;
+            LastWriteTimestamp = lastWrite;
+            VolumeID = volumeID;
+            VolumeLastWriteTimestamp = volumeLastWrite;
+        }
+    }
+}
--- AmcacheParser/Program.cs
+++ AmcacheParser/Program.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using AmcacheParser.Classes;
 using Fclp;
 using Microsoft.Win32;
-using erzReg = Registry;
 using NLog;
 using NLog.Config;
 using NLog.Fluent;
@@ -37,29 +36,7 @@ namespace AmcacheParser
         }
 
 
-        const int  ProductName = 0;
-        const int CompanyName = 0x1;
-        const int FileVersionNumber = 0x2;
-        const int LanguageCode = 0x3;
-        const int SwitchBackContext = 0x4;
-        const int FileVersionString = 0x5;
-        const int FileSize = 0x6;
-        const int PEHeaderSize = 0x7;
-        const int PEHeaderHash = 0x8;
-        const int PEHeaderChecksum = 0x9;
-        const int Unknown1 = 0xa;
-        const int Unknown2 = 0xb;
-        const int FileDescription = 0xc;
-        const int Unknown3 = 0xd;
-        const int CompileTime = 0xf;
-        const int Unknown4 = 0x10;
-        const int LastModified = 0x11;
-        const int Created = 0x12;
-        const int FullPath = 0x15;
-        const int Unknown5 = 0x16;
-        const int LastModified2 = 0x17;
-        const int ProgramID = 0x100;
-        const int SHA1 = 0x101;
+
 
        
 
@@ -132,249 +109,21 @@ namespace AmcacheParser
             _sw = new Stopwatch();
             _sw.Start();
 
-            var counter = 0;
 
             try
             {
-                var reg = new erzReg.RegistryHive(p.Object.File);
-                reg.RecoverDeleted = true;
-                reg.ParseHive();
+                _sw.Start();
 
-                var fileKey = reg.GetKey(@"Root\File");
-                var programsKey = reg.GetKey(@"Root\Programs");
-
-                var ProgramsEntries = new List<ProgramsEntry>();
-
-                //First, we get data for all the Program entries under Programs key
-
-                foreach (var registryKey in programsKey.SubKeys)
-                {
-                    var ProgramName0 = "";
-                    var ProgramVersion1 = "";
-                    var Guid10 = "";
-                    var UninstallGuid11 = "";
-                    var Guid12 = "";
-                    var Dword13 = 0;
-                    var Dword14 = 0;
-                    var Dword15 = 0;
-                    var UnknownBytes = new byte[0];
-                    long Qword17 = 0;
-                    var Dword18 = 0;
-                    var VenderName2 = "";
-                    var LocaleID3 = "";
-                    var Dword5 = 0;
-                    var InstallSource6 = "";
-                    var UninstallKey7 = "";
-                    DateTimeOffset EpochA = DateTimeOffset.FromUnixTimeSeconds(0);
-                    DateTimeOffset EpochB = DateTimeOffset.FromUnixTimeSeconds(0);
-                    var PathListd = "";
-                    var Guidf = "";
-                    var RawFiles = "";
-
-                    foreach (var value in registryKey.Values)
-                    {
-                        switch (value.ValueName)
-                        {
-                            case "0":
-                                ProgramName0 = value.ValueData;
-                                break;
-                            case "1":
-                                ProgramVersion1 = value.ValueData;
-                                break;
-                            case "2":
-                                VenderName2 = value.ValueData;
-                                break;
-                            case "3":
-                                LocaleID3 = value.ValueData;
-                                break;
-                            case "5":
-                                Dword5 = int.Parse(value.ValueData);
-                                break;
-                            case "6":
-                                InstallSource6 = value.ValueData;
-                                break;
-                            case "7":
-                                UninstallKey7 = value.ValueData;
-                                break;
-                            case "a":
-                                EpochA = DateTimeOffset.FromUnixTimeSeconds(long.Parse(value.ValueData));
-
-                                break;
-                            case "b":
-                                EpochB =
-                               DateTimeOffset.FromUnixTimeSeconds(long.Parse(value.ValueData));
-                                break;
-                            case "d":
-                                PathListd = value.ValueData;
-                                break;
-                            case "f":
-                                Guidf = value.ValueData;
-                                break;
-                            case "10":
-                                Guid10 = value.ValueData;
-                                break;
-                            case "11":
-                                UninstallGuid11 = value.ValueData;
-                                break;
-                            case "12":
-                                Guid12 = value.ValueData;
-                                break;
-                            case "13":
-                                Dword13= int.Parse(value.ValueData);
-                                break;
-                            case "14":
-                                Dword13 = int.Parse(value.ValueData);
-                                break;
-                            case "15":
-                                Dword13 = int.Parse(value.ValueData);
-                                break;
-                            case "16":
-                                UnknownBytes = value.ValueDataRaw;
-                                break;
-                            case "17":
-                                Qword17 = long.Parse(value.ValueData);
-                                break;
-                            case "18":
-                                Dword18 = int.Parse(value.ValueData);
-                                break;
-                            case "Files":
-                                RawFiles = value.ValueData;
-                                break;
-                            default:
-                                _logger.Warn($"Unknown value name in Program: {value.ValueName}");
-                                break;
-                        }
-                    }
-
-                        var pe = new ProgramsEntry(ProgramName0, ProgramVersion1, VenderName2, LocaleID3, InstallSource6, UninstallKey7, Guid10, Guid12, UninstallGuid11, Dword5, Dword13, Dword14, Dword15, UnknownBytes, Qword17, Dword18, EpochA, EpochB, PathListd, Guidf, RawFiles,registryKey.KeyName,registryKey.LastWriteTime.Value);
-
-                        ProgramsEntries.Add(pe);
-                  
-                 }
-
-                //Dump stuff from Programs to a file for testing, etc
-                //ProgramsEntries.ForEach(f=>File.AppendAllText(@"C:\temp\programs.txt", $"{f.ProgramName_0} EpochA: {f.InstallDateEpoch_a} EpochB: {f.InstallDateEpoch_a}\r\n"));
-
-
-                //For each Programs entry, add the related Files entries from Files\Volume subkey
-
-                var unassociatedFiles = new List<FileEntry>();
-
-                foreach (var registryKey in fileKey.SubKeys)
-                {
-                    //These are the guids for volumes
-                    foreach (var subKey in registryKey.SubKeys)
-                    {
-                        counter += 1;
-
-                        var prodName = "";
-                        var fullPath = "";
-                        var progID = "";
-                        var sha = "";
-                        DateTimeOffset lm2 = new DateTimeOffset(1970,1,1,0,0,0,TimeSpan.FromHours(0));
-
-                        var hasLinkedProgram = false;
-
-                        //these are the files executed from the volume
-                        foreach (var keyValue in subKey.Values)
-                        {
-                            var keyVal = int.Parse(keyValue.ValueName, NumberStyles.HexNumber);
-
-                            switch (keyVal)
-                            {
-                                case ProductName:
-                                    prodName = keyValue.ValueData;
-                                    break;
-                                case CompanyName:
-                                    break;
-                                case FileVersionNumber:
-                                    break;
-                                case LanguageCode:
-                                    break;
-                                case SwitchBackContext:
-                                    break;
-                                case FileVersionString:
-                                    break;
-                                case FileSize:
-                                    break;
-                                case PEHeaderSize:
-                                    break;
-                                case PEHeaderHash:
-                                    break;
-                                case PEHeaderChecksum:
-                                    break;
-                                case Unknown1:
-                                    break;
-                                case Unknown2:
-                                    break;
-                                case FileDescription:
-                                    break;
-                                case Unknown3:
-                                    break;
-                                case CompileTime:
-                                    break;
-                                case Unknown4:
-                                    break;
-                                case LastModified:
-                                    break;
-                                case Created:
-                                    break;
-                                case FullPath:
-                                    fullPath = keyValue.ValueData;
-                                    break;
-                                case Unknown5:
-                                    break;
-                                case LastModified2:
-                                    lm2 = DateTimeOffset.FromFileTime(long.Parse(keyValue.ValueData));
-                                    break;
-                                case ProgramID:
-                                    progID = keyValue.ValueData;
-
-                                    var program = ProgramsEntries.SingleOrDefault(t => t.ProgramID == progID);
-                                    if (program != null)
-                                    {
-                                        //_logger.Info(program.ProgramVersion_1);
-                                        hasLinkedProgram = true;
-                                    }
-//                                    else
-//                                    {
-//                                        _logger.Error($"{subKey.KeyName} has no linking Program value");
-//                                    }
-                                    
-                                    break;
-                                case SHA1:
-                                    sha = keyValue.ValueData;
-                                    break;
-                                default:
-                                    _logger.Warn($"Unknown value name: 0x{keyVal:X}");
-                                    break;
-                            }
-                        }
-
-                        var fe = new FileEntry(prodName, progID, sha, fullPath, lm2,registryKey.KeyName,registryKey.LastWriteTime.Value,subKey.KeyName,subKey.LastWriteTime.Value);
-
-                        if (hasLinkedProgram)
-                        {
-                            var program = ProgramsEntries.SingleOrDefault(t => t.ProgramID == fe.ProgramID);
-                            program.FileEntries.Add(fe);
-                        }
-                        else
-                        {
-                            unassociatedFiles.Add(fe);
-                        }
-
-                        
-
-                    }
-                }
-
+                var am = new Amcache.Amcache(p.Object.File);
+                
                 _sw.Stop();
-
-                var suffix = counter == 1 ? "" : "s";
+                
+                var suffix = am.UnassociatedFileEntries.Count == 1 ? "y" : "ies";
 
                 _logger.Info("");
                 _logger.Info(
-                    $"Found {counter:N0} string{suffix} in {_sw.Elapsed.TotalSeconds:N3} seconds.");
+                    $"Found {am.UnassociatedFileEntries.Count:N0} unassociated file entr{suffix} and {am.ProgramsEntries.Count:N0} program entries in {_sw.Elapsed.TotalSeconds:N3} seconds.");
+                
 
             }
             catch (Exception ex)
@@ -411,6 +160,8 @@ namespace AmcacheParser
     {
         public string File { get; set; }
         public string Extension { get; set; } = string.Empty;
+        public bool Whitelist { get; set; } = false;
+        public bool IncludeLinked { get; set; } = true;
 
     }
 }
