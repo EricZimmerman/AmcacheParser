00bacb95205a3e50c7ab2b85b8f8934f7b5e1cbc
Eric Zimmerman <saericzimmerman@gmail.com>
2015-08-02 12:19:50 -0600

    0.0.5.2

--- Amcache/Properties/AssemblyInfo.cs
+++ Amcache/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Amcache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Parses Amcache hives")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eric Zimmerman")]
 [assembly: AssemblyProduct("Amcache")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("saericzimmerman@gmail.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
